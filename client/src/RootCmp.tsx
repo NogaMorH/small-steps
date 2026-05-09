6a7fd26b6b2b6c80c648da32cb10e4031a5fe373
Noga Mor-Havilio <nogamorh@gmail.com>
2026-05-09 18:22:26 +0300

    Add Basic routing infrastructure

---
+++ client/src/RootCmp.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router'
+import { HomePage } from './pages/HomePage'
+
+export function RootCmp() {
+    return (
+        <div className="main-container">
+            <main>
+                <Routes>
+                    <Route path="" element={<HomePage />} />
+                </Routes>
+            </main>
+        </div>
+    )
+}
